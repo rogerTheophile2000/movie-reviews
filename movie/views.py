d805f778f9c02ae4c8f7338a1a03a6e82242c2ec
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-05-23 22:26:42 +0300

    adding movie detail part

--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Movie
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -28,3 +29,6 @@ def signup(request):
     email = request.GET.get('email')
     return render(request, 'signup.html', {'email':email})
 
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie,pk=movie_id)
+    return render(request, 'detail.html', {'movie':movie})
