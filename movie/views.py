a941c6bcb3ebb808b2e61cf98d4ded4707f91afe
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-05-23 15:39:33 +0300

    adding template on view, and displaying bootstrap in django

--- movie/views.py
+++ movie/views.py
@@ -3,8 +3,21 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
+
+
+
+
+
+
 def home(request):
-    return HttpResponse('<h1>Welcome to Home Page</h1>')
+    searchTerm = request.GET.get('searchMovie')
+    return render(request, 'home.html', {'searchTerm':searchTerm})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email':email})
+
