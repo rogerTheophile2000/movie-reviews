4e27f854e918cbf2fffa12945bb34a3e75233204
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-05-23 18:19:02 +0300

    adding movie model and learn how adding our models in admin pannel

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from movie import views as movieViews
 
 urlpatterns = [
@@ -23,4 +25,6 @@ urlpatterns = [
     path('', movieViews.home, name='home'),
     path('about/', movieViews.about, name='about'),
     path('signup/', movieViews.signup, name='signup'),
-]
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
